ab47c19c958bbdc3301faa9b636c006de114e306
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-07 14:26:12 -0800

    styling

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,55 +3,49 @@ import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 import './Hero.css'
 
-import HeroLogo from '../../assets/heroLogo.png'
+// import BgVideo from '../../assets/hero-vbg.mp4'
+
+import waves from '../../assets/waves.svg'
 
 
 
 const Hero = () => {
     return (
-        <>
+<>        
+
+
+
         <div className='hero'>
-        {/* <video src={BgVideo} autoPlay muted loop playsInline className='video-bg' />             */}
+
+        <div className='hero-divider'>
+                <img src={waves} loading="lazy" repeat-x alt='wave divider'/>
+            </div>
+
+
             <div className="hero-container">
                 <div className="content">
-
-                <div className='hero-logo'>
+                        <h1 className="animate-charcter">BUENA PARK <br/> REHAB</h1>
                     <Fade left>
-                    <img src={HeroLogo} width="550px" height="250px" alt="Buena Park Logo" loading="lazy"/>
+                        <h2 className='subtitle'>Substance Abuse Addiction and <br/>Mental Health Treatment Facility</h2>
                     </Fade>
+                        <br />
+                        <div className='hero-btn'>
+                        <Link to="/contact">
+                        <button>Get an Appointment</button>
+                    </Link>
                     </div>
-                    
-                <Fade top><h1>BUENA PARK</h1></Fade> 
-                <Fade bottom>
-                    <h2 className='subtitle'>SUBSTANCE ABUSE AND MENTAL HEALTH TREATMENT FACILITY</h2>
-                    <p>Find your happiness and learn to put it first.</p>
-                </Fade>
-
-                    <Link to="/treatment"><button>Learn More</button></Link>
+                    <br />
+                    <Fade bottom>
+                        <p>Find your happiness and learn to put it first.</p>
+                    </Fade>
                 </div>
             </div>
         </div>
 
 
-        </>
+</>
+
     )
 }
 
 export default Hero
-
-
-// .hero {
-//     width: 100%;
-//     height: 80vh;
-//     background: rgba(0, 0, 0, .4);
-//     position: relative;
-// }
-
-// .hero:before {
-//     content: '';
-//     position: absolute;
-//     background: url('../../assets/hero-image.webp') no-repeat center center/cover;
-//     height: 100%;
-//     width: 100%;
-//     z-index: -1;
-// }
--- src/components/servicesCards/Cards.js
+++ src/components/servicesCards/Cards.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Cards.css'
-// import ServicesCards from '../props/CardProps';
 
-// import Fade from 'react-reveal/Fade';
+import { Link } from 'react-router-dom'
+import { Link as LinkRoll } from 'react-scroll'
+
+import Fade from 'react-reveal/Fade';
 
 
 import Card1 from '../../assets/card1.jpg'
@@ -13,67 +15,67 @@ import Card3 from '../../assets/card3.jpg'
 const Cards = () => {
   return (
     <>
+
             <div className="cards-header container">
                 <h1>WE OFFER VARIOUS SERVICES </h1>
-                <br/>
-                <p>At Buena Park Rehab Center, clients receive individual therapy alongside a range of additional treatments, including relapse prevention, trauma therapy, mindfulness meditation, transportation, yoga, and more. We offer support that clients need to allow them to gradually go back to treatment. </p>
+                <p>At Buena Park Rehab Treatment Center, clients receive individual therapy alongside a range of additional treatments, including relapse prevention, trauma therapy, mindfulness meditation, transportation, yoga, and more. We offer support that clients need to allow them to gradually go back to treatment. </p>
             </div>
+<div class="card-wrap">
+<div class="tile"> 
+  <Fade left>
+  <img src={Card1} alt="Mental Health" loading="lazy"/>
+  </Fade>
+  <div class="text">
+  <h1>MENTAL HEALTH TREATMENT​</h1>
+  <p class="animate-text">Some of our clients are here to attend to their mental health, and some come to us for help with addiction and sobriety. Depending on your needs, we offer treatment for co-occurring disorders that can address both mental health and addiction disorders at the same time.</p>
+    <div class="animate-text">
+    <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+        <Link to="/mental-health">
+            <button>Learn More</button>
+        </Link>
+    </LinkRoll>
+    </div>
+  </div>
+ </div>
 
-            <div className='cards-container'>
-                <div className='card'>
-                    <img className='card-image' src={Card1} alt="Mental Health" loading="lazy"/>
-                    <div className='intro'>
-                        <h1 className='card-title'>MENTAL HEALTH TREATMENT</h1>
-                        <p className='card-desc'>Some of our clients are here to attend to their mental health, and some come to us for help with addiction and sobriety. Depending on your needs, we offer treatment for co-occurring disorders that can address both mental health and addiction disorders at the same time.</p>
-                    </div>
-                </div>
-            
+<div class="tile"> 
+<Fade top>
+<img src={Card2} alt="Substance Use" loading="lazy"/>
+</Fade>
+  <div class="text">
+  <h1>SUBSTANCE ABUSE TREATMENT</h1>
+  <p class="animate-text">No matter what type of addiction or mental health condition you are dealing with, there’s something for everyone at Palm Springs Rehab.</p>
+  <div class="animate-text">
+    <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+        <Link to="/substance-abuse">
+            <button>Learn More</button>
+        </Link>
+    </LinkRoll>
+    </div>
 
-                <div className='card'>
-                    <img className='card-image' src={Card2} alt="Mental Health" loading="lazy"/>
-                    <div className='intro'>
-                        <h1 className='card-title'>SUBSTANCE ABUSE TREATMENT</h1>
-                        <p className='card-desc'>No matter what type of addiction or mental health condition you are dealing with, there’s something for everyone at Contra Costa Rehab Center.</p>
-                    </div>
-                </div>
+  </div>
+ </div>
+  
+  <div class="tile"> 
 
-                <div className='card'>
-                    <img className='card-image' src={Card3} alt="Mental Health" loading="lazy"/>
-                    <div className='intro'>
-                        <h1 className='card-title'>EMPLOYMENT ASSITANCE</h1>
-                        <p className='card-desc'>We offer a variety of employment tools and resources to help you land on your feet and get hired at a great company in recovery.</p>
-                    </div>
-                </div>
-            </div>
+  <Fade right>
+  <img src={Card3} alt="Warehouse Employees" loading="lazy"/>
+  </Fade>
+  <div class="text">
+  <h1>EMPLOYMENT ASSISTANCE</h1>
+  <p class="animate-text">We offer a variety of employment tools and resources to help you land on your feet and get hired at a great company in recovery.</p>
+  <div class="animate-text">
+    <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+        <Link to="/jobs">
+            <button>Learn More</button>
+        </Link>
+    </LinkRoll>
 
-
-        {/* <div className='cards'>
-            <div className='card-container'>
-                <Fade bottom>
-                    <ServicesCards
-                        Image={Card1}
-                        CardName="MENTAL HEALTH TREATMENT​"
-                        Description="Clients suffer from both mental health disorders and addiction. Therefore, we provide treatment for co-occurring disorders, addressing both conditions simultaneously."
-                    />
-                </Fade>
-
-                <Fade top>
-                    <ServicesCards
-                        Image={Card2}
-                        CardName="SUBSTANCE USE TREATMENT"
-                        Description="We treat a multitude of substance use disorders. Whether the client is suffering from an alcohol addiction, prescription drug addiction, we can help."
-                    />
-                </Fade>
-
-                <Fade bottom>
-                    <ServicesCards
-                        Image={Card3}
-                        CardName="EMPLOYMENT / JOB ASSISTANCE"
-                        Description="There are a variety of programs and organizations that fall under the umbrella of employment-based recovery support services. Jobs Assistance is available."
-                    />
-                </Fade>
-            </div>
-        </div> */}
+    </div>
+  </div>
+ </div>
+</div>
+  
     </>
   )
 }
